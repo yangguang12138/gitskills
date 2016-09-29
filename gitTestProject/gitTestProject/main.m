39561116ba3485d2712a173e9e7128860eb78e58
iOS_Onion <ios_onion@Mannys-MacPro.local>
2016-09-29 16:08:52 +0800

    gitTestProject

---
+++ gitTestProject/gitTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTestProject
+//
+//  Created by iOS_Onion on 16/9/29.
+//  Copyright © 2016年 iOS_Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
