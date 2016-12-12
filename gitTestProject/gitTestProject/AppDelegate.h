abb980c3919aa7f094188e9b7391e647f8418b2d
iOS_Onion <ios_onion@Mannys-MacPro.local>
2016-12-12 17:12:05 +0800

    nil

---
+++ gitTestProject/gitTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTestProject
+//
+//  Created by iOS_Onion on 16/9/29.
+//  Copyright © 2016年 iOS_Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTestProject/gitTestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitTestProject
+//
+//  Created by iOS_Onion on 16/9/29.
+//  Copyright © 2016年 iOS_Onion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
